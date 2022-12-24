edee832f1b25df321cd7b7d38285f4198386cd2f
JeekLee <j3kkun@gmail.com>
2022-12-24 15:02:04 +0900

    refactor: 쓸데 없는 주석 삭제

--- src/main/java/com/hanghae/instagram/common/response/SuccessResponse.java
+++ src/main/java/com/hanghae/instagram/common/response/SuccessResponse.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import java.time.LocalDateTime;
 
 @Getter
-// 1. Response를 Builder Pattern을 이용해 ResponseEntity로 변환하기 위해 선언
 @Builder
 public class SuccessResponse {
     private final LocalDateTime timestamp = LocalDateTime.now();
@@ -15,7 +14,6 @@ public class SuccessResponse {
     private final String code;
     private final String message;
 
-    // 2. Response를 Response Entity로 변환하는 Method
     public static ResponseEntity<SuccessResponse> toResponseEntity(SuccessCode successCode) {
         return ResponseEntity
                 .status(successCode.getHttpStatus())
