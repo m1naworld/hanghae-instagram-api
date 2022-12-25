142962912a8780080020662441a83ef72c7fb455
mina <m1nna@naver.com>
2022-12-25 16:12:04 +0900

    feat: PropertyValueException 핸들러 추가

--- src/main/java/com/hanghae/instagram/common/exception/ErrorResponse.java
+++ src/main/java/com/hanghae/instagram/common/exception/ErrorResponse.java
@@ -2,18 +2,14 @@ package com.hanghae.instagram.common.exception;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import java.time.LocalDateTime;
 
 @Getter
 // 1. ErrorResponse를 Builder Pattern을 이용해 ResponseEntity로 변환하기 위해 선언
 @Builder
 public class ErrorResponse {
-    private final LocalDateTime timestamp = LocalDateTime.now();
     private final int status;
-    private final String error;
-    private final String code;
     private final String message;
 
     // 2. ErrorResponse를 Response Entity로 변환하는 Method
@@ -22,10 +18,19 @@ public class ErrorResponse {
                 .status(errorCode.getHttpStatus())
                 .body(ErrorResponse.builder()
                         .status(errorCode.getHttpStatus().value())
-                        .error(errorCode.getHttpStatus().name())
-                        .code(errorCode.name())
                         .message(errorCode.getMessage())
                         .build()
                 );
     }
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(HttpStatus status, String message ) {
+        return ResponseEntity
+                .status(status)
+                .body(ErrorResponse.builder()
+                        .status(status.value())
+                        .message(message)
+                        .build()
+                );
+    }
+
 }
