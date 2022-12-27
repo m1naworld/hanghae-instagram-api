75f4a520051bdd73ff878b878dc3cf67b9bbc3f0
seungit <minsg3669@gmail.com>
2022-12-28 02:12:12 +0900

    feat : 댓글 수정 API

--- src/main/java/com/hanghae/instagram/comment/controller/CommentController.java
+++ src/main/java/com/hanghae/instagram/comment/controller/CommentController.java
@@ -5,10 +5,14 @@ import com.hanghae.instagram.comment.dto.ResponseComment;
 import com.hanghae.instagram.comment.service.CommentService;
 import com.hanghae.instagram.common.response.DataResponse;
 import com.hanghae.instagram.common.response.SuccessCode;
+import com.hanghae.instagram.member.entity.Member;
+import com.hanghae.instagram.security.userdetails.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +24,17 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<?> createComment(@PathVariable Long postingId, @RequestBody RequestComment requestDto) {
-//    Member member = memberRepository.getMemberById;  // 씨큐리티 적용
-    ResponseComment responseComment = commentService.createComment(postingId, requestDto);
-    return DataResponse.toResponseEntity(SuccessCode.CREATE_COMMENT_SUCCESS, responseComment);
-}
+    public ResponseEntity<?> createComment(@PathVariable Long postingId, @RequestBody RequestComment requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Member loginMember = userDetails.getMember();
+        ResponseComment responseComment = commentService.createComment(postingId, requestDto, loginMember);
+        return DataResponse.toResponseEntity(SuccessCode.CREATE_COMMENT_SUCCESS, responseComment);
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<?> editComment(@PathVariable Long postingId, @PathVariable Long commentId, @RequestBody RequestComment requestDto,
+                                                     @AuthenticationPrincipal UserDetailsImpl userDetails){
+        Member loginMember = userDetails.getMember();
+        ResponseComment responseComment = commentService.editComment(postingId, commentId, requestDto, loginMember);
+        return DataResponse.toResponseEntity(SuccessCode.CREATE_COMMENT_SUCCESS, responseComment);
+    }
 }
